43dcaa04bee67467a9693ac05b91fc32997c2e4e
root <root@dev-autobox.lab.ansfederal.com>
2020-10-13 12:54:49 -0400

    creating sample command

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,28 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'cml-terminals',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension cml-terminals is activated!');
+
+    const { commands } = app;
+    const command = 'deploy-lab:command'
+
+    //Add Deploy Lab Command
+    commands.addCommand(command, {
+      label: 'Deploy Lab deploy-lab:command Command',
+      caption: 'Deploy deploy-lab:command Command',
+      execute: (args: any) => {
+        const orig = args['origin'];
+        console.log('deploy-lab:command has been called from %{orig}.');
+        if (orig !== 'init') {
+          window.alert('deploy-lab:command hass been called from ${orig}.');
+        }
+      }
+    });
+
+    commands.execute(command, { origin: 'init' }).catch(reason => {
+      console.error(
+        'An error occured during the execution of deploy-lab:command.\n${reason}'
+      );
+    });
   }
 };
 
